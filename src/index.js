82df95b6a3782185bce7e723d3b28827da9e0c0b
deniskodak <denis.kodak@gmail.com>
2021-11-19 18:59:58 +0200

    remove bg from section to section:after

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ import './js/scrollTo';
 import './js/customSelect';
 import './js/inputsAnimation';
 import './js/buildSelect';
+
+$(document).ready(function () {
+  $('section[data-type="background"]').each(function () {
+    var $bgobj = $(this); // создаем объект
+    $(window).scroll(function () {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент
+      // Присваиваем значение background-position
+      var coords = 'center ' + yPos + 'px';
+      // Создаем эффект Parallax Scrolling
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
+});
